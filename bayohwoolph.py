ea8a4e423ae3b91cd1bea82460350df52a45d4a2
Eric Eisenhart <freiheit@gmail.com>
2017-02-05 13:07:44 -0800

    Initial bare-minimum running bot.

---
+++ bayohwoolph.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import asyncio
+import configparser
+import discord
+import os
+from discord.ext import commands
+
+# Parse the config and stick in global "config" var
+config = configparser.ConfigParser()
+for inifile in [os.path.expanduser('~')+'/.bayohwoolph.ini','bayohwoolph.local.ini','bayohwoolph.ini']:
+    if os.path.isfile(inifile):
+        config.read(inifile)
+        break # First config file wins
+MAIN = config['MAIN']
+
+description = '''Dark Echo's barkeep'''
+bot = commands.Bot(command_prefix='%', description=description)
+
+@bot.event
+@asyncio.coroutine
+def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+bot.run(MAIN.get('login_token'))
