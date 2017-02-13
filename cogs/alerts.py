28ed09efc9019e72b28a2f327a602c9c68daaf33
freiheit <freiheit@gmail.com>
2017-02-12 21:59:08 -0800

    Alert to high-command when somebody leaves the server.

--- cogs/alerts.py
+++ cogs/alerts.py
@@ -16,6 +16,13 @@ class Alerts:
     def on_member_join(self, member):
         lobby = self.bot.get_channel(LOBBY)
         yield from self.bot.send_message(lobby,"Salutations {0.mention}! Welcome to Dark Echo's Discord server. Please speak up and <@&146724062301913088> will get your roles sorted shortly.".format(member))
+
+    @asyncio.coroutine
+    def on_member_remove(self, member):
+        highcommand = self.bot.get_channel(HIGH_COMMAND)
+        msg = '<@&146724062301913088> {0.mention} left.'
+
+        yield from self.bot.send_message(highcommand,msg.format(member))
         
 def setup(bot):
     bot.add_cog(Alerts(bot))
