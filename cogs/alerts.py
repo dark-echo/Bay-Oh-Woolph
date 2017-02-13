69e4396bad537a85285d5c3e420e4c7c6f535ef6
freiheit <freiheit@gmail.com>
2017-02-12 20:19:23 -0800

    Fix mentions

--- cogs/alerts.py
+++ cogs/alerts.py
@@ -15,7 +15,7 @@ class Alerts:
     @asyncio.coroutine
     def on_member_join(self, member):
         lobby = self.bot.get_channel(LOBBY)
-        yield from self.bot.send_message(lobby,"Salutations {0.mention}! Welcome to Dark Echo's Discord server. Please speak up and <@146724062301913088> will get your roles sorted shortly.".format(member))
+        yield from self.bot.send_message(lobby,"Salutations {0.mention}! Welcome to Dark Echo's Discord server. Please speak up and <@&146724062301913088> will get your roles sorted shortly.".format(member))
         
 def setup(bot):
     bot.add_cog(Alerts(bot))
