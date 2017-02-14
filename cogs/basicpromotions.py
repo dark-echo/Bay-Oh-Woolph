7cb8d8c47c4f4761a57da1b855d75dc56166d16a
InfiniteMaximum <daniel.dod40@gmail.com>
2017-02-13 19:26:13 -0800

    Changed method to allow for multiple roles implying "OR"

--- cogs/basicpromotions.py
+++ cogs/basicpromotions.py
@@ -53,8 +53,7 @@ class Basicpromotions:
         self.bot = bot
         
     @commands.command()
-    @commands.has_role('Leadership')
-    @commands.has_role('Recruiter')
+    @commands.has_any_role('Leadership','Recruiter')
     @asyncio.coroutine
     def newcadet(self,
         member1  : discord.Member = None, 
