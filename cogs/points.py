7c57a679330f7300f465f0f9767511f2fdcf6170
Daniel O'Donnell <Daniel.dod40@gmail.com>
2017-02-18 23:00:57 -0500

    Fix Indenting

--- cogs/points.py
+++ cogs/points.py
@@ -10,23 +10,23 @@ class Points:
         self.bot = bot
 
 #Test method to populate an array from discord -Infinite
-@bot.command()
-@asyncio.coroutine
-def getmembers():
+    @bot.command()
+    @asyncio.coroutine
+    def getmembers(self):
     #Intialize array
-    listOfMembers = [] 
+        listOfMembers = [] 
 
-    #Add members to array
-    for member in self.bot.get_all_members():
-        listOfMembers.append(Member(parseStr(member.id),member.name,member.nick,member.top_role,0))
+        #Add members to array
+        for member in self.bot.get_all_members():
+            listOfMembers.append(Member(parseStr(member.id),member.name,member.nick,member.top_role,0))
 
-    #Fetch the a member at [x] index added to the array.
-    sentence = listOfMembers[8].displayMember()
-    #Get length of array.
-    length = len(listOfMembers)
+        #Fetch the a member at [x] index added to the array.
+        sentence = listOfMembers[8].displayMember()
+        #Get length of array.
+        length = len(listOfMembers)
 
-    yield from self.bot.say(str(sentence))
-    yield from self.bot.say("NumberofMembersinArray: "+str(length))
+        yield from self.bot.say(str(sentence))
+        yield from self.bot.say("NumberofMembersinArray: "+str(length))
 
 def setup(bot):
     bot.add_cog(Points(bot))
