df80d11fd8a2950b3fb7849e2e619313475e28a8
Daniel O'Donnell <Daniel.dod40@gmail.com>
2017-02-24 22:52:15 -0500

    Added checkin event WIP.

--- cogs/points.py
+++ cogs/points.py
@@ -2,9 +2,10 @@ from discord.ext import commands
 from utils import *
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import exists
+from sqlalchemy import *
 from member import Base, Member
 from utility.dbproc import Baydb
+import _datetime
 import discord
 import asyncio
 
@@ -25,6 +26,8 @@ class Points:
 
     ROLE_MEMBER = '284029774513569793'
 
+    global conn
+    conn = Baydb.conn
 
 
     #Add points command
@@ -70,7 +73,57 @@ class Points:
             else:
                 yield from self.bot.say(" No points for you imposter... ")
 
-                # Add points command
+    # Event Checkin
+    @commands.command(pass_context=True)
+    @asyncio.coroutine
+    def checkin(self,ctx):
+        """Adds points to specified member."""
+
+        amember = ctx.message.author
+        storemember = []
+        points = 0
+        #date
+
+
+
+
+
+
+
+
+        for member in session.query(Member). \
+                filter(Member.id == amember.id):
+            storemember.append(member)
+
+        if storemember:
+            points = storemember[0].points
+
+            points = points + 1
+
+            current_time = _datetime.datetime.utcnow()
+
+            dateparams = [null, current_time + _datetime.timedelta(weeks=1)]
+
+            checkdate = select([Member.checkInDate], Member.checkInDate.in_(dateparams))
+
+            result = conn.execute(checkdate)
+
+            if checkdate == dateparams[0] or checkdate == dateparams[1]:
+                yield from self.bot.type()
+
+                session.query(Member).filter_by(id=amember.id).update({"points": points})
+                session.commit()
+                session.close()
+
+                yield from self.bot.say(
+                    "You have gained a checkin point {0.mention}!".format(
+                        amember) + " Total points: " + str(
+                        points))
+
+        else:
+            yield from self.bot.say(" No points for you imposter... ")
+
+
 
 
 
