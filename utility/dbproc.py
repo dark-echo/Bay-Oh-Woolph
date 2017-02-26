d44010acc32fcb78570cd34478d0f4e8f1cfa979
freiheit <freiheit@gmail.com>
2017-02-26 15:31:58 -0800

    Move another usage of DB into ini file thing.

--- utility/dbproc.py
+++ utility/dbproc.py
@@ -3,13 +3,14 @@ from utils import *
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from member import Base, Member
+from config import Config
 import discord
 import asyncio
 
 
 class Baydb:
 
-    engine = create_engine('sqlite:///bayohwoolph.db')
+    engine = create_engine(Config.MAIN['dbpath'])
     Base.metadata.bind = engine
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
