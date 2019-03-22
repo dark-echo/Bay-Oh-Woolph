82454ff327bba705a90423d9b062a240937a642a
pmjustice91 <pmjustice91@gmail.com>
2019-03-21 22:45:18 -0500

    updated to only control the Mg tag assignment

--- cogs/mgmanager.py
+++ cogs/mgmanager.py
@@ -10,35 +10,27 @@ logger = logging.getLogger('bayohwoolph.cogs.mgmanager')
 MULTIGAMETAGS = Config.config['MULTIGAMETAGS']
 
 ROLE_MULTIGAME = MULTIGAMETAGS['ROLE_MULTIGAME']
-ROLE_APEX = MULTIGAMETAGS['ROLE_APEX']
-ROLE_WOW = MULTIGAMETAGS['ROLE_WOW']
-ROLE_TCTD2 = MULTIGAMETAGS['ROLE_TCTD2']
-ROLE_FORTNITE = MULTIGAMETAGS['ROLE_FORTNITE']
-ROLE_ROE = MULTIGAMETAGS['ROLE_ROE']
-ROLE_SOT = MULTIGAMETAGS['ROLE_SOT']
-ROLE_MINECRAFT = MULTIGAMETAGS['ROLE_MINECRAFT']
-
-
 
 
 
 
 Welcome_Message = """Congrats for joining the Multi-Game side of Dark Echo!!!
-Below you will find a list of games that we have tags for. If you don't see a tag then please ping <@&551503870048862226 with the name of the game and we can look to add it.
+Below you will find a list of games that we have tags for. If you don't see a tag then please ping <@&551503870048862226> with the name of the game and we can look to add it.
 
-Minecraft
-Sea Of Thieves
-Ring of Elysium
-Fortnite
-Apex Legends
-The Division 2
-World of Warships
+Please use the following commands to add game tags:
+$Minecraft
+$SeaOfThieves
+$RingOfElysium
+$Fortnite
+$ApexLegends
+$TheDivision2
+$WorldOfWarships
 
 
-Please use $"gamename" to add game tags to yourself doing so allows you to see any game specific channels.
-
-Any questions please ping <@&551503870048862226 with your question and we will get back to you asap.
+Any questions please ping <@&551503870048862226> with your question and we will get back to you asap.
 """
+
+
 
 
 class Multigame:
@@ -48,6 +40,7 @@ class Multigame:
 
     @commands.command()
     async def Multigame(self, ctx):
+        """Adds Multigame tags"""
 
         mg_role = discord.utils.get(ctx.guild.roles, id=int(ROLE_MULTIGAME))
         member = ctx.message.author
@@ -57,6 +50,8 @@ class Multigame:
             await ctx.send(Welcome_Message)
         except:
             await ctx.send('Error Unable to add role')
+
+
 
 
 
